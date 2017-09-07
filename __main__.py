44a223ce580e7b816803068da38baf376a6eebbc
Ben Trachtenberg <btr1975@gmail.com>
2017-09-07 13:45:51 -0500

    Update __main__.py
    Added option for using only a vars file

--- __main__.py
+++ __main__.py
@@ -12,21 +12,23 @@ __version_info__ = (1, 0, 5, __status__)
 __version__ = '.'.join(map(str, __version_info__))
 __maintainer__ = 'Benjamin P. Trachtenberg'
 __email__ = 'e_ben_75-python@yahoo.com'
+COMPILE = False
 
 
 LOGGER = logging.getLogger(__name__)
 
 
 def set_directory_structure():
-    if __status__ == 'prod':
+    if COMPILE:
         return mod.Directories(base_dir=os.path.dirname(os.path.realpath(sys.argv[0])))
 
-    elif __status__ == 'dev':
+    else:
         return mod.Directories(base_dir=os.path.dirname(os.path.realpath(__file__)))
 
 
 if __name__ == '__main__':
     directories = set_directory_structure()
+    yml_file = None
 
     logging.basicConfig(format='%(asctime)s: %(name)s - %(levelname)s - %(message)s',
                         filename=os.path.join(directories.logging_dir, 'logs.txt'))
@@ -34,7 +36,11 @@ if __name__ == '__main__':
     LOGGER.warning('Logging started!!')
 
     arg_parser = ArgumentParser(description='Quick Config Templates')
-    arg_parser.add_argument('yaml_file', help='The name of the yml file of your config')
+    arg_parser.add_argument('yml_file', nargs='*', help='The name of the yml file of your config, '
+                                                        'not required if you are using the -a option')
+    arg_parser.add_argument('-a', '--auto_build', action='store_true',
+                            help='This option uses a csv only to build a config.  If you use this option you '
+                                 'are also required to use the -t option.')
     arg_parser.add_argument('-c', '--config_only', action='store_true', help='Display the config, do not output '
                                                                              'to file')
     arg_parser.add_argument('-d', '--debug', help='The debug level default is level 3, highest is 1 and lowest is 5, '
@@ -62,7 +68,23 @@ if __name__ == '__main__':
         else:
             output_file_name = 'config.txt'
 
-        mod.scripts.te(directories, mod.scripts.pre_run_yml(directories.get_yml_dir(), args.yaml_file, args.typefile),
+        if args.auto_build:
+            if args.typefile:
+                yml_file = mod.auto_build_check(directories, args.typefile)
+
+            else:
+                arg_parser.print_help()
+                exit('\n!!! You are not using the -a option correctly please see the help. !!!')
+
+        else:
+            if len(args.yml_file) == 1:
+                yml_file = args.yml_file[0]
+
+            else:
+                arg_parser.print_help()
+                exit('\n!!! You are required to have only one yml_file argument. !!!')
+
+        mod.scripts.te(directories, mod.scripts.pre_run_yml(directories.get_yml_dir(), yml_file, args.typefile),
                        output_file_name, args.config_only, args.json, args.yml)
 
     except AttributeError as e:
