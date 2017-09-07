e77e57a77726c8edbad15466189b41083ef5725c
Ben Trachtenberg <btr1975@gmail.com>
2017-09-07 13:47:06 -0500

    Update __init__.py
    auto_build_check

--- module/scripts/__init__.py
+++ module/scripts/__init__.py
@@ -1,3 +1,4 @@
 from .template_engine import run_template as te
 from .template_engine import pre_run_yml_input_file as pre_run_yml
+from .template_engine import auto_build_template as auto_build_check
 from .directories_class import Directories
