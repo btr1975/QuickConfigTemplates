ac08b4df32edd417198ad1983e51c6494123d12b
Ben <btr1975@gmail.com>
2021-05-17 14:50:23 -0500

    Moving to version 2.0.0

--- __main__.py
+++ __main__.py
@@ -5,11 +5,11 @@ import platform
 from argparse import ArgumentParser
 import module as mod
 __author__ = 'Benjamin P. Trachtenberg'
-__copyright__ = "Copyright (c) 2018 Ben Trachtenberg"
+__copyright__ = "Copyright (c) 2018 - 2021 Ben Trachtenberg"
 __credits__ = 'Benjamin P. Trachtenberg'
 __license__ = 'MIT'
 __status__ = 'prod'
-__version_info__ = (1, 0, 16, __status__)
+__version_info__ = (2, 0, 0)
 __version__ = '.'.join(map(str, __version_info__))
 __maintainer__ = 'Benjamin P. Trachtenberg'
 __email__ = 'e_ben_75-python@yahoo.com'
