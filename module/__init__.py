68e6a17d856bbb4a72e163e2532f2ec98b735ad7
Ben <btr1975@gmail.com>
2021-05-17 14:44:50 -0500

    Checking new versions of libraries

--- module/__init__.py
+++ module/__init__.py
@@ -3,5 +3,6 @@ from .scripts import ServerTemplateEngine
 from .scripts import Directories
 from .scripts import convert_pl, convert_rm, convert_acl
 from .scripts import ARestMe
-from .scripts import run_local_server
+#  FIXME: Issue moving to flask 2.0.0
+# from .scripts import run_local_server
 from .utils import custom_filters
--- module/scripts/__init__.py
+++ module/scripts/__init__.py
@@ -5,4 +5,5 @@ from .prefix_list_convert import convert_prefix_list_to_our_format as convert_pl
 from .route_map_convert import convert_route_map_to_our_format as convert_rm
 from .acl_convert import convert_acl_to_our_format as convert_acl
 from .arestme import ARestMe
-from .qct_server import run_local_server
+#  FIXME: Issue moving to flask 2.0.0
+# from .qct_server import run_local_server
