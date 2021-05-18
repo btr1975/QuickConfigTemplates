4364d846446d3c6744e37e4f72ba7bf6264a581c
Benjamin Trachtenberg <btr1975@gmail.com>
2021-05-17 20:33:11 -0500

    Refactor for linting

--- module/scripts/directories_class.py
+++ module/scripts/directories_class.py
@@ -93,6 +93,14 @@ class Directories:  # pylint: disable=too-many-instance-attributes
             self.logging_level = logging.WARNING
 
     def set_logging_level(self, level):
+        """Method to set the logging level
+
+        :type level: Integer
+        :param level: The logging level
+
+        :rtype: None
+        :returns: Nonr
+        """
         if str(level) == '1':
             self.logging_level = logging.DEBUG
         elif str(level) == '2':
@@ -105,16 +113,42 @@ class Directories:  # pylint: disable=too-many-instance-attributes
             self.logging_level = logging.CRITICAL
 
     def get_logging_level(self):
+        """Method to get current logging level
+
+        :rtype: Integer
+        :returns: The logging level
+        """
         return self.logging_level
 
     def set_output_dir_folder(self, output_folder):
+        """Method to set the output directory
+
+        :type output_folder: String
+        :param output_folder: The folder to output to
+
+        :rtype: None
+        :returns: None
+        """
         pdt.verify_directory(output_folder, self.output_dir, directory_create=True)
         self.output_dir = os.path.join(self.output_dir, output_folder)
 
     def get_output_dir(self):
+        """Method to get the output directory
+
+        :rtype: String
+        :returns: The output directory
+        """
         return self.output_dir
 
     def get_yml_dir(self, file_name):
+        """Method to get file path
+
+        :type file_name: String
+        :param file_name: The filename to look for
+
+        :rtype: String
+        :returns: The directory where the file is located
+        """
         for yml_dir in self.yml_dir:
             if os.path.isfile(os.path.join(yml_dir, file_name)):
                 return yml_dir
@@ -123,12 +157,27 @@ class Directories:  # pylint: disable=too-many-instance-attributes
         raise FileNotFoundError(error)
 
     def get_templates_dir(self):
+        """Method to get the templates directory
+
+        :rtype: String
+        :returns: The templates directory
+        """
         return self.templates_dir
 
     def get_data_dir(self):
+        """Method to get the data directory
+
+        :rtype: String
+        :returns: The data directory
+        """
         return self.data_dir
 
     def get_logging_dir(self):
+        """Method to get the logging directory
+
+        :rtype: String
+        :returns: The logging directory
+        """
         return self.logging_dir
 
     def collect_and_zip_files(self, dir_list, zip_file_name, file_extension_list=None,  # pylint: disable=too-many-locals,too-many-branches
